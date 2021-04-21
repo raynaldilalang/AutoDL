e6b7f3a64046e297d0244785d21f8ea480c68def
raynaldilalang <raynaldilalang@gmail.com>
2021-04-21 15:47:16 +0700

    modify testing config

--- for_testing_binary_gpu_one.py
+++ for_testing_binary_gpu_one.py
@@ -8,9 +8,9 @@ from hyperband import HyperBandTorchSearchCV
 from Models import MLPClassifier
 from utils import *
 from sklearn.metrics import roc_auc_score, accuracy_score, f1_score, precision_score, recall_score
-from multiprocessing.pool import Pool
+# from multiprocessing.pool import Pool
 
-def main(x):
+def child(z):
     device='cuda'
     config={'list_hidden_layer': [16, 32, 64, 64, 32], 'input_size': 11, 'output_size': 1, 'batch_size': 256, 'epoch': 10, 'activation': 'ReLU', 'optimizer': 'Adam', 'lr': 0.013100488985051721, 'drop_rate': 0.020882414665211624, 'l1': 0.00024315820298144713, 'l2': 0.056070623526488214, 'batch_norm': True, 'device': device}
 
@@ -25,7 +25,11 @@ def main(x):
     print(model.predict(X))
     print(model.predict_proba(X))
 
+def parent(x):
+    with MyPool(1) as p:
+        p.map(child, [1, 2])
+
 if __name__ == "__main__":
-    with Pool(1) as p:
-        p.map(main, [1, 2, 3])
+    with MyPool(1) as p:
+        p.map(parent, [1, 2])
     # cross_val_score_torch(model, X, y, roc_auc_score, cv=5, n_jobs=5, device='cuda')
